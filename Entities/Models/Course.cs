c775b620a043e056b4b138a8758cb0f80ebfea28
MohammadAli Sedaghat <mohammadali.sedaghat.1999@gmail.com>
2024-12-20 18:54:24 +0330

    feat(entities): add course model

---
+++ Entities/Models/Course.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models;
+
+public class Course
+{
+    [Required]
+    public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(120, ErrorMessage = "Maximum length of Name is 120")]
+    public required string Name { get; set; }
+
+    [Required]
+    [MaxLength(60, ErrorMessage = "Maximum length of Instructor is 60")]
+    public required string Instructor { get; set; }
+
+    public DateTime CreatedAt { get; init; } = DateTime.Now;
+}
