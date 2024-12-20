ce6a0033a9672a01ab2b857ea88b351d402a6d9d
MohammadAli Sedaghat <mohammadali.sedaghat.1999@gmail.com>
2024-12-20 18:53:44 +0330

    feat(entities): add student model

---
+++ Entities/Models/Student.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models;
+
+public class Student
+{
+    [Required]
+    public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(60, ErrorMessage = "Maximum length of FirstName is 60")]
+    public required string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(60, ErrorMessage = "Maximum length of LastName is 60")]
+    public required string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public required string Email { get; set; }
+
+    [Required]
+    public required string StdId { get; set; }
+
+    public DateOnly? BirthDate { get; set; }
+}
