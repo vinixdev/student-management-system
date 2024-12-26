0e197945746bc2a7893180e86dd7577ee3472e3c
MohammadAli Sedaghat <mohammadali.sedaghat.1999@gmail.com>
2024-12-26 14:39:33 +0330

    chore(Repository): add project Repository and configure repository context

---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,17 @@
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository;
+
+public class RepositoryContext : DbContext
+{
+
+    public RepositoryContext() { }
+
+
+    public DbSet<Student> Students { set; get; }
+    public DbSet<Course> Courses { set; get; }
+    public DbSet<Enrollment> Enrollments { set; get; }
+
+
+}
