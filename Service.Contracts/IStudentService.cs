e5b80cfb0921247955986caeea2288bbc1a0012a
MohammadAli Sedaghat <mohammadali.sedaghat.1999@gmail.com>
2025-01-01 17:22:54 +0330

    chore(Service): add GetAllStudets as contract for services

--- Service.Contracts/IStudentService.cs
+++ Service.Contracts/IStudentService.cs
@@ -1,8 +1,9 @@
 using System;
+using Shared.DataTransferObjecs;
 
 namespace Service.Contracts;
 
 public interface IStudentService
 {
-
+    Task<IEnumerable<StudentDto>> GetAllStudents(bool trackChanges);
 }
