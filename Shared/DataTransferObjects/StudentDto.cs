003eb1685d9dcc66a87aef0c19586931b9c2604b
MohammadAli Sedaghat <mohammadali.sedaghat.1999@gmail.com>
2025-01-02 16:07:16 +0330

    chore: add properties for Student DTOs

--- Shared/DataTransferObjects/StudentDto.cs
+++ Shared/DataTransferObjects/StudentDto.cs
@@ -4,5 +4,10 @@ namespace Shared.DataTransferObjects;
 
 public record StudentDto
 {
-
+    public Guid Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public DateOnly? BirthDate { get; set; }
+    // TODO: Display courses in which a student is enrolled
 }
--- Shared/DataTransferObjects/StudentForCreationDto.cs
+++ Shared/DataTransferObjects/StudentForCreationDto.cs
@@ -1,3 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Shared.DataTransferObjects;
 
-public record StudentForCreationDto();
+public record StudentForCreationDto
+{
+    
+    [Required]
+    [MaxLength(60, ErrorMessage = "Maximum length of FirstName is 60")]
+    public required string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(60, ErrorMessage = "Maximum length of LastName is 60")]
+    public required string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public required string Email { get; set; }
+}
