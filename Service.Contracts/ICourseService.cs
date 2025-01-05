140009f6df518dbeef858454c761cb9b36d7b115
MohammadAli Sedaghat <mohammadali.sedaghat.1999@gmail.com>
2025-01-05 10:03:53 +0330

    chore: add service contract for get all courses

--- Service.Contracts/ICourseService.cs
+++ Service.Contracts/ICourseService.cs
@@ -1,8 +1,9 @@
 using System;
+using Shared.DataTransferObjects;
 
 namespace Service.Contracts;
 
 public interface ICourseService
 {
-    
+    Task<IEnumerable<CourseDto>> GetAllCourses(bool trackChanges);
 }
