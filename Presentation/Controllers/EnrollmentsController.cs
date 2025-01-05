8aa237567282a5c60e5a434bfc00d1592b343580
MohammadAli Sedaghat <mohammadali.sedaghat.1999@gmail.com>
2025-01-05 17:45:44 +0330

    feat(EnrollmentController): add create enrollment for student in a course

--- Presentation/Controllers/EnrollmentsController.cs
+++ Presentation/Controllers/EnrollmentsController.cs
@@ -1,8 +1,24 @@
-using System;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using Shared.DataTransferObjects;
 
 namespace Presentation.Controllers;
-
-public class EnrollmentsController
+[ApiController]
+[Route("api/students/{studentId:guid}/courses/{courseId:guid}")]
+public class EnrollmentsController: ControllerBase
 {
+    private readonly IServiceManager _service;
+    public EnrollmentsController(IServiceManager service) => _service = service;
 
+    [HttpPost]
+    public async Task<IActionResult> StudentEnrollCourse(Guid studentId, Guid courseId)
+    {
+        var enrollmentForCreationDto = new EnrollmentForCreationDto();
+        enrollmentForCreationDto.StudentId = studentId;
+        enrollmentForCreationDto.CourseId = courseId;
+
+        await _service.Enrollment.CreateEnrollment(enrollmentForCreationDto);
+
+        return NoContent();
+    }
 }
