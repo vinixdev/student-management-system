7c959a997c4d7b8134d00b5c309c6e87df3c46f8
MohammadAli Sedaghat <mohammadali.sedaghat.1999@gmail.com>
2025-01-05 15:54:00 +0330

    chore(EnrollmentService): add contract for create Enrollment

--- Service.Contracts/IEnrollmentService.cs
+++ Service.Contracts/IEnrollmentService.cs
@@ -1,8 +1,9 @@
 using System;
+using Shared.DataTransferObjects;
 
 namespace Service.Contracts;
 
 public interface IEnrollmentService
 {
-
+    Task CreateEnrollment(EnrollmentForCreationDto enrollmentForCreationDto);
 }
