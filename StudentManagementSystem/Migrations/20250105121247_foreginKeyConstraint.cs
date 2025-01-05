f2a18ac74c1ef3a4821b235466c28ebdf2523ab8
MohammadAli Sedaghat <mohammadali.sedaghat.1999@gmail.com>
2025-01-05 15:45:32 +0330

    chore(Main Project): make migration for foreginkey constraint

---
+++ StudentManagementSystem/Migrations/20250105121247_foreginKeyConstraint.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class foreginKeyConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "Score",
+                table: "Enrollments",
+                type: "smallint",
+                nullable: true,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+
+            migrationBuilder.CreateTable(
+                name: "CourseStudent",
+                columns: table => new
+                {
+                    CoursesId = table.Column<Guid>(type: "uuid", nullable: false),
+                    StudentsId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseStudent", x => new { x.CoursesId, x.StudentsId });
+                    table.ForeignKey(
+                        name: "FK_CourseStudent_Courses_CoursesId",
+                        column: x => x.CoursesId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CourseStudent_Students_StudentsId",
+                        column: x => x.StudentsId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollments_CourseId",
+                table: "Enrollments",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollments_StudentId",
+                table: "Enrollments",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseStudent_StudentsId",
+                table: "CourseStudent",
+                column: "StudentsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollments_Courses_CourseId",
+                table: "Enrollments",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollments_Students_StudentId",
+                table: "Enrollments",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollments_Courses_CourseId",
+                table: "Enrollments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollments_Students_StudentId",
+                table: "Enrollments");
+
+            migrationBuilder.DropTable(
+                name: "CourseStudent");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Enrollments_CourseId",
+                table: "Enrollments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Enrollments_StudentId",
+                table: "Enrollments");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "Score",
+                table: "Enrollments",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (byte)0,
+                oldClrType: typeof(byte),
+                oldType: "smallint",
+                oldNullable: true);
+        }
+    }
+}
