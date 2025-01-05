5928c6f6b709ada1112a263983df2c7995ca6179
MohammadAli Sedaghat <mohammadali.sedaghat.1999@gmail.com>
2025-01-05 10:13:33 +0330

    feat(CourseController): add get all courses

--- Presentation/Controllers/CoursesController.cs
+++ Presentation/Controllers/CoursesController.cs
@@ -1,8 +1,21 @@
 using System;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
 
 namespace Presentation.Controllers;
-
-public class CoursesController
+[ApiController]
+[Route("api/courses")]
+public class CoursesController: ControllerBase
 {
+    private readonly IServiceManager _service;
 
+    public CoursesController(IServiceManager service) => _service = service;
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllCourses()
+    {
+        var courses = await _service.Course.GetAllCourses(trackChanges: false);
+
+        return Ok(courses);
+    }
 }
