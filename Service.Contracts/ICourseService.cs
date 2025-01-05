a8eebff34f89d581dc6f17feca146c1f404a68c1
MohammadAli Sedaghat <mohammadali.sedaghat.1999@gmail.com>
2025-01-05 12:42:36 +0330

    chore(CourseService): add contract for update coure

--- Service.Contracts/ICourseService.cs
+++ Service.Contracts/ICourseService.cs
@@ -9,4 +9,5 @@ public interface ICourseService
     Task<CourseDto> GetCourseAsync(Guid courseId, bool trackChanges);
     Task<CourseDto> CreateCourseAsync(CourseForCreationDto courseForCreationDto);
     Task DeleteCourseAsync(Guid courseId);
+    Task<CourseDto> UpdateCourseAsync(Guid courseId, CourseForUpdateDto courseForUpdateDto, bool trackChanges);
 }
