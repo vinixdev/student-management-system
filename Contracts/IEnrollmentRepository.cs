0e5472171a1712bd0e07bfab15c8c0bd2ed9e349
MohammadAli Sedaghat <mohammadali.sedaghat.1999@gmail.com>
2025-01-06 18:03:50 +0330

    chore(EnrollmentRepository): add contract for get enrollments by courseId

--- Contracts/IEnrollmentRepository.cs
+++ Contracts/IEnrollmentRepository.cs
@@ -7,8 +7,6 @@ public interface IEnrollmentRepository
     void CreateEnrollment(Enrollment enrollment);
     Task<Enrollment?> GetEnrollmentAsync(Guid studentId, Guid courseId, bool trackChanges);
     Task<IEnumerable<Enrollment>> GetEnrollmentsByStudentIdAsync(Guid studentId, bool trackChanges);
+    Task<IEnumerable<Enrollment>> GetEnrollmentsByCourseIdAsync(Guid courseId, bool trackChanges);
     void DeleteEnrollment(Enrollment enrollment);
-    
-
-    // TODO: create route for courses to get students they enroll
 }
