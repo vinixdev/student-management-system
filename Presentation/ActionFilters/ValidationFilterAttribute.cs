81942d3c12b3d470ba0f401b80d804520dde5d93
MohammadAli Sedaghat <mohammadali.sedaghat.1999@gmail.com>
2025-01-06 19:54:34 +0330

    chroe(presentation): add custom action filter

---
+++ Presentation/ActionFilters/ValidationFilterAttribute.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Presentation.ActionFilters;
+
+public class ValidationFilterAttribute: IActionFilter
+{
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        if (!context.ModelState.IsValid)
+        {
+            context.Result = new UnprocessableEntityObjectResult(context.ModelState);
+        }
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+    }
+}
